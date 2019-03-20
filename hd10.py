078a1b84891574aa18123601e208d423bf79ab69
eok <37176818+eokristjans@users.noreply.github.com>
2019-03-20 18:02:54 +0000

    Dæmi 3 nýtist í 3ja lið í V2

--- hd10.py
+++ hd10.py
@@ -5,13 +5,43 @@ Adaptive Quadrature as given in Sauer uses the Trapezoid rule to define
 S_[a,b] = 0.5*(b-a)(f(a)+f(b))
 Here we will use Simpson's rule
 Look better into pages 269-270 in the textbook
+Modify the Matlab code for Adaptive Trapezoid Rule Quadrature to use Simpson’s Rule
+instead, applying the criterion (5.42) with the 15 replaced by 10. Approximate the integral in
+Example 5.12 within 0.005, and compare with Figure 5.5(b). How many subintervals were
+required?
 
-I guess we'll basically need 
-
-
+|S[a,b] − (S[a,c] + S[c,b])| < 15 ∗ TOL
 """
-def SimpsonAdaptiveQuadrature(f, x0, x2, tol):
-    x1 = 0.5*(x0+x2)
+# Just a function that calls another function
+def _AdaptiveIntegration(f, a, b, tol):
+    fa = f(a)
+    fb = f(b)
+    c, fc, sab = _SimpsonsRule(f, a, fa, b, fb)
+    return _AdQuadSimpson(f, a, fa, b, fb, tol, sab, c, fc)
+
+def _SimpsonsRule(f, a, fa, b, fb):
+    h = (b-a)/6
+    c = (a+b)/2
+    fc = f(c)
+    return (c, fc, h * (fa + 4 * fc + fb))
+
+def _AdQuadSimpson(f, a, fa, b, fb, tol, sab, c, fc):
+    h = (b-a)/6
+    lc, flc, sac  = _SimpsonsRule(f, a, fa, c, fc)
+    rc, frc, scb = _SimpsonsRule(f, c, fc, b, fb)
+    if abs(sab - sac - scb) < 15*tol and h < 1e-1:
+        return sac + scb
+    return (_AdQuadSimpson(f, a, fa, c, fc, tol/2, sac , lc, flc) +
+           _AdQuadSimpson(f, c, fc, b, fb, tol/2, scb, rc, frc))
+
+# Dæmi um keyrslu
+g = lambda x: 1 + math.sin(math.exp(3*x))
+ans = _AdaptiveIntegration(g,-1,1,0.005)
+print(ans)
+# Gefur 2.5010131869874983
+
+
+
 
 """ Dæmi 1 """
 def RombergIntegration(f, a, b, n):
