b22b4a0c05204107387c1983bbba7ee13aac102c
Davíð Freyr Björnsson <david24099@gmail.com>
2019-03-24 11:31:28 +0000

    lessFun

--- v2/functionsV2.py
+++ v2/functionsV2.py
@@ -3,25 +3,21 @@
 Functions used in v2
 @author: Erling Oskar
 """
-# To install autograd run pip install autograd
-# Note: pylint incorrectly gives errors on 
-# the autograd.numpy module and grad function
-import autograd.numpy as np
-from autograd import *
+import numpy as np
 
 """ SA 1 """
 # Returns the lamda function sqrt(dxdt(t)^2 + dydt(t)^2)
 # given x(t) and y(t)
-def sqrtFunSquared(x, y):
-    dxdt = grad(x)
-    dydt = grad(y)
+def sqrtFunSquared(x, y, diffMethod):
+    dxdt = diffMethod(x)
+    dydt = diffMethod(y)
     return lambda t: np.sqrt(dxdt(t)**2 + dydt(t)**2)
 
 # Computes the arc length for a given function (f)
 # and tolerance (tol), from t = 0 to t = T
 # with a given method of integration. 
-def compArcLength(f, T, intMethod, tol = 0.5e-8):
-    return intMethod(f, 0, T, tol)
+def compArcLength(f, t, T, intMethod, tol):
+    return intMethod(f, t, T, tol)
 
 # Computes the definite integral of a given function (f)
 # from a to b using the method of Adaptive Quadrature
@@ -31,11 +27,17 @@ def adQuad(f, a, b, tol):
     S_ab = (h/2)*(f(a)+f(b))
     S_ac = (h/4)*(f(a)+f(c))
     S_cb = (h/4)*(f(c)+f(b))
+    print("Tol er ", tol)
+    print(np.abs(S_ab - S_cb - S_ac))
     if np.abs(S_ab - S_cb - S_ac) < 3*tol: #  and h < 1e-2
         return (S_ac + S_cb)
     else:
-        return (adQuad(f, a, c, tol/2) +
-                adQuad(f, c, b, tol/2))
+        return (adQuad(f, a, c, tol/2) + adQuad(f, c, b, tol/2))
+
+# Returns a function that approximates of the derivative of f(x)
+# at x = a
+def threePointCentDiff(f, h=1e-10):
+    return lambda a: (f(a+h) - f(a-h))/(2*h)
 
 """ SA 2 """
 # Usage:    BisectionMethod(f,a,b)
@@ -43,7 +45,7 @@ def adQuad(f, a, b, tol):
 #           a < b are numbers such that f(a)*f(b) < 0
 #           tol is a number
 # Post:     c is the root of f, i.e f(c) = 0
-def bisectionMethod(f,a,b,tol=0.5e-6):
+def bisectionMethod(f,a,b,tol):
     fa=f(a)
     fb=f(b)
     while (b-a)/2 > tol:
@@ -57,10 +59,10 @@ def bisectionMethod(f,a,b,tol=0.5e-6):
             fa=fc
     return c
 
-def tStarOfS(f, s, intMethod, rootMethod):
-    a = compArcLength(f, 1, intMethod)
-    g = lambda b: (s * a - compArcLength(f, b, intMethod))
-    return rootMethod(g, 0, 1)
+def tStarOfS(f, s, intMethod, rootMethod, tol):
+    a = compArcLength(f, 0.0, 1.0, intMethod, tol)
+    g = lambda b: (s * a - compArcLength(f, 0.0, b, intMethod, tol))
+    return rootMethod(g, 0.0, 1.0, tol)
 
 """ SA 3 """
 def simpsonsRule(f, a, b):
@@ -68,7 +70,7 @@ def simpsonsRule(f, a, b):
     c = (a+b)/2
     return (c, h * (f(a) + 4 * f(c) + f(b)))
 
-def adQuadSimpson(f, a, b, tol = 0.5e-8, c=None):
+def adQuadSimpson(f, a, b, tol, c=0):
     c, sab = simpsonsRule(f, a, b)
     h = (b-a)/6
     lc, sac  = simpsonsRule(f, a, c)
@@ -79,10 +81,13 @@ def adQuadSimpson(f, a, b, tol = 0.5e-8, c=None):
            adQuadSimpson(f, c, b, tol, rc))
     
 """ SA 4 """
-def newtonsMethod(f, xold, tol=0.5e-4):
-    dfdt = grad(f)
+
+'''
+def newtonsMethod(f, xold, tol):
+    dfdt = threePointCentDiff(f)
     xnew = xold - f(xold)/dfdt(xold)
     while abs(xnew-xold) > tol:
         xold = xnew
         xnew = xold - f(xold)/dfdt(xold)
     return xnew
+'''
--- v2/v2.py
+++ v2/v2.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from functionsV2 import *
-import autograd.numpy as np
-import time, timeit
+import numpy as np
+from time import perf_counter
+
+# Temporary global tol
+tol = 0.5e-5
 
 """ Suggested Activity 1 """
 # Given parametric path with
@@ -12,86 +15,75 @@ y = lambda t: 1.5 + 0.3*t + 0.9*t**2 - 2.7*t**3
 
 # Compute the lambda function that
 # is to be integrgated
-f = sqrtFunSquared(x, y)
-print(f(1.0))
-'''
+f = sqrtFunSquared(x, y, threePointCentDiff)
+
 # Compute the corresponding arc length
 # by computing the integral of f from
 # 0 to 1 using the method of Adaptive Quadrature
-arcLength = compArcLength(f, 1.0, adQuad)
+arcLength = compArcLength(f, 0.0, 1.0, adQuad, tol)
 
-print("The arc length is", arcLength)
+print("The arc length is", round(arcLength, 5))
 
 """ Suggested Activity 2 """
 # Let's time the function tStarOfS
 # for s = 0.5 
 s = 0.5
-start = time.perf_counter()
-tStar = tStarOfS(f, s, adQuad, bisectionMethod)
-end = time.perf_counter()
-time = end - start
+start = perf_counter()
+tStar2 = tStarOfS(f, s, adQuad, bisectionMethod, tol)
+end = perf_counter()
+elapsedTime2 = end - start
 
-print('The optimal value of t for ' + str(s) + ' is ' + str(tStar))
-print('and was computed in', time, " seconds")
+print('The optimal value of t for ' + str(s) + ' is ' + str(tStar2))
+print('and was computed in', round(elapsedTime2, 5), "seconds")
 
 # Let's verify if tStar really is the
 # root of the function 
-print('Verified:', (np.abs(compArcLength(f, tStar, adQuad) / 
-                           compArcLength(f, 1, adQuad) - s) < 0.001))
+print('Verified:', (np.abs(compArcLength(f, 0.0, tStar2, adQuad, tol) / 
+                           compArcLength(f, 0.0, 1.0, adQuad, tol) - s) < 0.001))
 
 """ Suggested Activity 3 """
 # For n = 4
 sArray = [0.0, 0.25, 0.5, 0.75, 1.0]
 
-# Time the
-start = time.perf_counter()
-tStarArray = [tStarOfS(f, s, adQuadSimpson, bisectionMethod) for s in sArray]
-end = time.perf_counter()
-time = end - start 
+start = perf_counter()
+tStarArray = [tStarOfS(f, s, adQuadSimpson, bisectionMethod, tol) for s in sArray]
+end = perf_counter()
+elapsedTime3n4 = end - start
+print("Í SA3 fyrir n = 4 þá tók keyrslan ", elapsedTime3n4, " sekúndur")
 
-for i in range(len(sArray)):
-    arclength = compArcLength(f, tStarArray[i+1], tStarArray[i])
+for i in range(len(sArray)-1):
+    arclength = compArcLength(f, tStarArray[i], tStarArray[i+1], adQuadSimpson, tol)
     print('Arclength from', round(tStarArray[i]), end='')
     print(' to ', round(tStarArray[i+1],2), end='') 
-    print('is:', round(arclength,2), end='')
+    print('is ', round(arclength,2))
     print('Proportional arc length :', end='')
-    print(round(np.abs(arclength / compArcLength(f, 1, adQuadSimpson)), 2))
+    print(round(np.abs(arclength / compArcLength(f, 0.0, 1.0, adQuadSimpson, tol)), 2))
 
 # For n = 20
 sArray = np.arange(0.00, 1.05, 0.05)
-time_SA3_n20 = time.perf_counter()
-ts3n20 = [tStarOfS_SR(f_SA1, s) for s in s3n20]
-time_SA3_n20 = time.perf_counter() - time_SA3_n20
-
-print("SA#, n = 20")
-for i in range(20):
-    arclength = ComputeArcLengthSR(f_SA1, ts3n20[i+1], ts3n20[i])
-    print('Arclength from', round(ts3n20[i], 2), ' to ', round(ts3n20[i+1], 2), 'is:', round(arclength,2))
+start = perf_counter()
+tStarArray = [tStarOfS(f, s, adQuadSimpson, bisectionMethod, tol) for s in sArray]
+end = perf_counter()
+elapsedTime3n20 = end - start
+print("Í SA3 fyrir n = 20 þá tók keyrslan ", elapsedTime3n20, " sekúndur")
+ 
+for i in range(len(sArray)-1):
+    arclength = compArcLength(f, tStarArray[i], tStarArray[i+1], adQuadSimpson, tol)
+    print('Arclength from', round(tStarArray[i], 2), ' to ', round(tStarArray[i+1], 2), 'is:', round(arclength,2))
     print('Proportional Arclength :', round(np.abs(arclength / 
-                           ComputeArcLengthSR(f_SA1, 1)), 2))
+                           compArcLength(f, 0.0, 1.0, adQuadSimpson, tol)), 2))
     
 """ Suggested Activity 4 """
-def tStarOfS_SR_Newt(f, s):
-    a = ComputeArcLengthSR(f, 1)
-    g = lambda b: (s * a - ComputeArcLengthSR(f, b))
-    return NewtonsMethod(g, 0, 1)
+'''
+start = perf_counter()
+tStar4 = tStarOfS(f, s, adQuad, newtonsMethod, tol)
+end = perf_counter()
+elapsedTime4 = end - start
 
-time0_SA4 = time.perf_counter()
-ts2_SA4 = tStarOfS_SR_Newt(f_SA1, s2)
-time0_SA4 = time.perf_counter() - time0_SA4
+print("Í SA4 þá tók keyrslan ", elapsedTime4, " sekúndur")
 
-print(ts2_SA4)
-print(ts2)
-print('The optimal value of t for ',s2, 
-      ' is equal to the one from Suggest Activity 4:', (abs(ts2_SA4-ts2)<1e-5))
+print('The optimal value of t for ', s, 
+      ' is equal to the one from Suggest Activity 4:', (abs(tStar4-tStar2)<1e-5))
 print('Time required to compute t*(s) using the Trapezoid Rule and Newton\'s method',
-      'is less than with the Bisection Method:', (time0_SA4 < time_SA2) )
-
-
-
-time_SA2
-time0_SA4
-
-ts2_SA4
-ts2
+      'is less than with the Bisection Method:', (elapsedTime4 < elapsedTime2) )
 '''
